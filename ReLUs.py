4c97c18239d972dd3a9ea865d822dde5a3c119c6
diego <diegocaramuta@hotmail.com>
2017-07-11 14:54:31 -0400

    uy

---
+++ ReLUs.py
@@ -0,0 +1,259 @@
+import tensorflow as tf
+
+output = None
+hidden_layer_weights = [
+    [0.1, 0.2, 0.4],
+    [0.4, 0.6, 0.6],
+    [0.5, 0.9, 0.1],
+    [0.8, 0.2, 0.8]]
+out_weights = [
+    [0.1, 0.6],
+    [0.2, 0.1],
+    [0.7, 0.9]]
+
+# Weights and biases
+weights = [
+    tf.Variable(hidden_layer_weights),
+    tf.Variable(out_weights)]
+biases = [
+    tf.Variable(tf.zeros(3)),
+    tf.Variable(tf.zeros(2))]
+
+# Input
+features = tf.Variable([[1.0, 2.0, 3.0, 4.0], [-1.0, -2.0, -3.0, -4.0], [11.0, 12.0, 13.0, 14.0]])
+
+# TODO: Create Model
+hidden_layer = tf.add(tf.matmul(features, weights[0]), biases[0])
+hidden_layer = tf.nn.relu(hidden_layer)
+logits = tf.add(tf.matmul(hidden_layer, weights[1]), biases[1])
+
+# TODO: Print session results
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    print(sess.run(logits))
+
+
+
+
+#####Deep Neural Network in TensorFlow
+######You've seen how to build a logistic classifier using TensorFlow. 
+####Now you're going to see how to use the logistic classifier to build a deep neural network.
+
+
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets(".", one_hot=True, reshape=False)
+import tensorflow as tf
+
+# Parameters
+learning_rate = 0.001
+training_epochs = 20
+batch_size = 128  # Decrease batch size if you don't have enough memory
+display_step = 1
+
+n_input = 784  # MNIST data input (img shape: 28*28)
+n_classes = 10  # MNIST total classes (0-9 digits)
+n_hidden_layer = 256 # layer number of features
+#The variable n_hidden_layer determines the size of the hidden layer in the neural network. 
+##This is also known as the width of a layer.
+
+# Store layers weight & bias
+weights = {
+    'hidden_layer': tf.Variable(tf.random_normal([n_input, n_hidden_layer])),
+    'out': tf.Variable(tf.random_normal([n_hidden_layer, n_classes]))
+}
+biases = {
+    'hidden_layer': tf.Variable(tf.random_normal([n_hidden_layer])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+# tf Graph input
+x = tf.placeholder("float", [None, 28, 28, 1])
+y = tf.placeholder("float", [None, n_classes])
+
+x_flat = tf.reshape(x, [-1, n_input])
+#The MNIST data is made up of 28px by 28px images with a single channel. 
+#The tf.reshape() function above reshapes the 28px by 28px matrices in x into row vectors of 784px.
+
+# Hidden layer with RELU activation
+layer_1 = tf.add(tf.matmul(x_flat, weights['hidden_layer']),\
+    biases['hidden_layer'])
+layer_1 = tf.nn.relu(layer_1)
+# Output layer with linear activation
+logits = tf.add(tf.matmul(layer_1, weights['out']), biases['out'])
+##You've seen the linear function tf.add(tf.matmul(x_flat, weights['hidden_layer']), 
+##biases['hidden_layer']) before, also known as xw + b. Combining linear functions together 
+##using a ReLU will give you a two layer network.
+
+# Define loss and optimizer
+cost = tf.reduce_mean(\
+    tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)\
+    .minimize(cost)
+    
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    # Training cycle
+    for epoch in range(training_epochs):
+        total_batch = int(mnist.train.num_examples/batch_size)
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            # Run optimization op (backprop) and cost op (to get loss value)
+            sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+
+##The MNIST library in TensorFlow provides the ability to receive the dataset in batches. 
+##Calling the mnist.train.next_batch() function returns a subset of the training data.
+
+
+
+#######Save and Restore TensorFlow Models
+##Training a model can take hours. But once you close your TensorFlow session, you lose all 
+##the trained weights and biases. If you were to reuse the model in the future, you 
+##would have to train it all over again!
+##Fortunately, TensorFlow gives you the ability to save your progress using a class 
+##called tf.train.Saver. This class provides the functionality to save any tf.Variable to 
+##your file system.
+
+import tensorflow as tf
+
+# The file path to save the data
+save_file = './model.ckpt'
+
+# Two Tensor Variables: weights and bias
+weights = tf.Variable(tf.truncated_normal([2, 3]))
+bias = tf.Variable(tf.truncated_normal([3]))
+
+# Class used to save and/or restore Tensor Variables
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    # Initialize all the Variables
+    sess.run(tf.global_variables_initializer())
+
+    # Show the values of weights and bias
+    print('Weights:')
+    print(sess.run(weights))
+    print('Bias:')
+    print(sess.run(bias))
+
+    # Save the model
+    saver.save(sess, save_file)
+
+#Now that the Tensor Variables are saved, let's load them back into a new model.
+# Remove the previous weights and bias
+tf.reset_default_graph()
+
+# Two Variables: weights and bias
+weights = tf.Variable(tf.truncated_normal([2, 3]))
+bias = tf.Variable(tf.truncated_normal([3]))
+
+# Class used to save and/or restore Tensor Variables
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    # Load the weights and bias
+    saver.restore(sess, save_file)
+
+    # Show the values of weights and bias
+    print('Weight:')
+    print(sess.run(weights))
+    print('Bias:')
+    print(sess.run(bias))
+##You'll notice you still need to create the weights and bias Tensors in Python. 
+##The tf.train.Saver.restore() function loads the saved data into weights and bias.
+##Since tf.train.Saver.restore() sets all the TensorFlow Variables, you don't need to 
+##call tf.global_variables_initializer().
+
+##Let's see how to train a model and save its weights.
+##First start with a model:
+
+# Remove previous Tensors and Operations
+tf.reset_default_graph()
+
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+
+learning_rate = 0.001
+n_input = 784  # MNIST data input (img shape: 28*28)
+n_classes = 10  # MNIST total classes (0-9 digits)
+
+# Import MNIST data
+mnist = input_data.read_data_sets('.', one_hot=True)
+
+# Features and Labels
+features = tf.placeholder(tf.float32, [None, n_input])
+labels = tf.placeholder(tf.float32, [None, n_classes])
+
+# Weights & bias
+weights = tf.Variable(tf.random_normal([n_input, n_classes]))
+bias = tf.Variable(tf.random_normal([n_classes]))
+
+# Logits - xW + b
+logits = tf.add(tf.matmul(features, weights), bias)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(\
+    tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)\
+    .minimize(cost)
+
+# Calculate accuracy
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+##Let's train that model, then save the weights:
+import math
+
+save_file = './train_model.ckpt'
+batch_size = 128
+n_epochs = 100
+
+saver = tf.train.Saver()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    # Training cycle
+    for epoch in range(n_epochs):
+        total_batch = math.ceil(mnist.train.num_examples / batch_size)
+
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_features, batch_labels = mnist.train.next_batch(batch_size)
+            sess.run(
+                optimizer,
+                feed_dict={features: batch_features, labels: batch_labels})
+
+        # Print status for every 10 epochs
+        if epoch % 10 == 0:
+            valid_accuracy = sess.run(
+                accuracy,
+                feed_dict={
+                    features: mnist.validation.images,
+                    labels: mnist.validation.labels})
+            print('Epoch {:<3} - Validation Accuracy: {}'.format(
+                epoch,
+                valid_accuracy))
+
+    # Save the model
+    saver.save(sess, save_file)
+    print('Trained Model Saved.')
+
+##Let's load the weights and bias from memory, then check the test accuracy.
+saver = tf.train.Saver()
+
+# Launch the graph
+with tf.Session() as sess:
+    saver.restore(sess, save_file)
+
+    test_accuracy = sess.run(
+        accuracy,
+        feed_dict={features: mnist.test.images, labels: mnist.test.labels})
+
+print('Test Accuracy: {}'.format(test_accuracy))
+
