231928b66425b8f8316dd042ffa91d0ac95e0f98
diego <diegocaramuta@hotmail.com>
2017-07-13 11:25:07 -0400

    dsf

---
+++ convolution_networks.py
@@ -0,0 +1,249 @@
+
+### Convolution Networks - CONVNETS
+##Let's examine how to implement a CNN in TensorFlow.
+
+import tensorflow as tf
+
+# Output depth
+k_output = 64
+
+# Image Properties
+image_width = 10
+image_height = 10
+color_channels = 3
+
+# Convolution filter
+filter_size_width = 5
+filter_size_height = 5
+
+# Input/Image
+input = tf.placeholder(
+    tf.float32,
+    shape=[None, image_height, image_width, color_channels])
+
+# Weight and bias
+weight = tf.Variable(tf.truncated_normal(
+    [filter_size_height, filter_size_width, color_channels, k_output]))
+bias = tf.Variable(tf.zeros(k_output))
+
+# Apply Convolution
+conv_layer = tf.nn.conv2d(input, weight, strides=[1, 2, 2, 1], padding='SAME')
+# Add bias
+conv_layer = tf.nn.bias_add(conv_layer, bias)
+# Apply activation function
+conv_layer = tf.nn.relu(conv_layer)
+
+##The code above uses the tf.nn.conv2d() function to compute the convolution with weight 
+##as the filter and [1, 2, 2, 1] for the strides. TensorFlow uses a stride for each input 
+##dimension, [batch, input_height, input_width, input_channels]. We are generally always 
+##going to set the stride for batch and input_channels (i.e. the first and fourth element 
+##in the strides array) to be 1.
+
+###############################
+### MAX POOLING
+##Conceptually, the benefit of the max pooling operation is to reduce the size of the input, and
+##allow the neural network to focus on only the most important elements. Max pooling does this 
+##by only retaining the maximum value for each filtered area, and removing the remaining values.
+#TensorFlow provides the tf.nn.max_pool() function to apply max pooling to your convolutional layers.
+
+...
+conv_layer = tf.nn.conv2d(input, weight, strides=[1, 2, 2, 1], padding='SAME')
+conv_layer = tf.nn.bias_add(conv_layer, bias)
+conv_layer = tf.nn.relu(conv_layer)
+# Apply Max Pooling
+conv_layer = tf.nn.max_pool(
+    conv_layer,
+    ksize=[1, 2, 2, 1],
+    strides=[1, 2, 2, 1],
+    padding='SAME')
+
+##The tf.nn.max_pool() function performs max pooling with the ksize parameter as the size of 
+##the filter and the strides parameter as the length of the stride. 2x2 filters with a stride 
+##of 2x2 are common in practice.
+
+
+#######################################Convolutional Network in TensorFlow
+##It's time to walk through an example Convolutional Neural Network (CNN) in TensorFlow.
+###The structure of this network follows the classic structure of CNNs, which is a mix of 
+###convolutional layers and max pooling, followed by fully-connected layers.
+
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets(".", one_hot=True, reshape=False)
+
+import tensorflow as tf
+
+# Parameters
+learning_rate = 0.00001
+epochs = 10
+batch_size = 128
+
+# Number of samples to calculate validation and accuracy
+# Decrease this if you're running out of memory to calculate accuracy
+test_valid_size = 256
+
+# Network Parameters
+n_classes = 10  # MNIST total classes (0-9 digits)
+dropout = 0.75  # Dropout, probability to keep units
+
+# Store layers weight & bias
+weights = {
+    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+    'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
+    'out': tf.Variable(tf.random_normal([1024, n_classes]))}
+
+biases = {
+    'bc1': tf.Variable(tf.random_normal([32])),
+    'bc2': tf.Variable(tf.random_normal([64])),
+    'bd1': tf.Variable(tf.random_normal([1024])),
+    'out': tf.Variable(tf.random_normal([n_classes]))}
+    
+def conv2d(x, W, b, strides=1):
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+    x = tf.nn.bias_add(x, b)
+    return tf.nn.relu(x)
+
+def maxpool2d(x, k=2):
+    return tf.nn.max_pool(
+        x,
+        ksize=[1, k, k, 1],
+        strides=[1, k, k, 1],
+        padding='SAME')
+
+##In the code below, we're creating 3 layers alternating between convolutions and max 
+##pooling followed by a fully connected and output layer.
+
+def conv_net(x, weights, biases, dropout):
+    # Layer 1 - 28*28*1 to 14*14*32
+    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+    conv1 = maxpool2d(conv1, k=2)
+
+    # Layer 2 - 14*14*32 to 7*7*64
+    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    conv2 = maxpool2d(conv2, k=2)
+
+    # Fully connected layer - 7*7*64 to 1024
+    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+    fc1 = tf.nn.relu(fc1)
+    fc1 = tf.nn.dropout(fc1, dropout)
+
+    # Output Layer - class prediction - 1024 to 10
+    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+    return out
+
+#################Now let's run it!
+
+# tf Graph input
+x = tf.placeholder(tf.float32, [None, 28, 28, 1])
+y = tf.placeholder(tf.float32, [None, n_classes])
+keep_prob = tf.placeholder(tf.float32)
+
+# Model
+logits = conv_net(x, weights, biases, keep_prob)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(\
+    tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)\
+    .minimize(cost)
+
+# Accuracy
+correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# Initializing the variables
+init = tf. global_variables_initializer()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(epochs):
+        for batch in range(mnist.train.num_examples//batch_size):
+            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            sess.run(optimizer, feed_dict={
+                x: batch_x,
+                y: batch_y,
+                keep_prob: dropout})
+
+            # Calculate batch loss and accuracy
+            loss = sess.run(cost, feed_dict={
+                x: batch_x,
+                y: batch_y,
+                keep_prob: 1.})
+            valid_acc = sess.run(accuracy, feed_dict={
+                x: mnist.validation.images[:test_valid_size],
+                y: mnist.validation.labels[:test_valid_size],
+                keep_prob: 1.})
+
+            print('Epoch {:>2}, Batch {:>3} -'
+                  'Loss: {:>10.4f} Validation Accuracy: {:.6f}'.format(
+                epoch + 1,
+                batch + 1,
+                loss,
+                valid_acc))
+
+    # Calculate Test Accuracy
+    test_acc = sess.run(accuracy, feed_dict={
+        x: mnist.test.images[:test_valid_size],
+        y: mnist.test.labels[:test_valid_size],
+        keep_prob: 1.})
+    print('Testing Accuracy: {}'.format(test_acc))
+
+
+###################
+#######Let's now apply what we've learned to build real CNNs in TensorFlow. 
+#######In the below exercise, you'll be asked to set up the dimensions of the
+####### Convolution filters, the weights, the biases. 
+
+import tensorflow as tf
+import numpy as np
+
+# `tf.nn.conv2d` requires the input be 4D (batch_size, height, width, depth)
+# (1, 4, 4, 1)
+x = np.array([
+    [0, 1, 0.5, 10],
+    [2, 2.5, 1, -8],
+    [4, 0, 5, 6],
+    [15, 1, 2, 3]], dtype=np.float32).reshape((1, 4, 4, 1))
+X = tf.constant(x)
+
+
+def conv2d(input):
+    # Filter (weights and bias)
+    F_W = tf.Variable(tf.truncated_normal((2, 2, 1, 3)))
+    F_b = tf.Variable(tf.zeros(3))
+    strides = [1, 2, 2, 1]
+    padding = 'VALID'
+    return tf.nn.conv2d(input, F_W, strides, padding) + F_b
+
+out = conv2d(X)
+
+#########################
+########In the below exercise, you'll be asked to set up the dimensions of the pooling filters, 
+#######strides, as well as the appropriate padding. You should go over the TensorFlow documentation
+####### for tf.nn.max_pool(). Padding works the same as it does for a convolution.
+
+import tensorflow as tf
+import numpy as np
+
+# `tf.nn.max_pool` requires the input be 4D (batch_size, height, width, depth)
+# (1, 4, 4, 1)
+x = np.array([
+    [0, 1, 0.5, 10],
+    [2, 2.5, 1, -8],
+    [4, 0, 5, 6],
+    [15, 1, 2, 3]], dtype=np.float32).reshape((1, 4, 4, 1))
+X = tf.constant(x)
+
+def maxpool(input):
+    ksize = [1, 2, 2, 1]
+    strides = [1, 2, 2, 1]
+    padding = 'VALID'
+    return tf.nn.max_pool(input, ksize, strides, padding)
+    
+out = maxpool(X)
+
+
